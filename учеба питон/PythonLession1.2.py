485e0e32ab6950544cf62e6c0a075f4efdc73af1
Am4i <synergyam4i@gmail.com>
2023-05-01 12:22:00 +0300

    новое дз

---
+++ учеба питон/PythonLession1.2.py
@@ -0,0 +1,10 @@
+#"Dryopithecus", "Ramapithecus", "Australopithecus", "Homo Erectus", "Homo Sapiens Neanderthalensis", "Homo Sapiens Sapiens"
+stage1 = input("Enter the first stage of human development: ")
+stage2 = input("Enter the second stage of human development: ")
+stage3 = input("Enter the third stage of human development: ")
+stage4 = input("Enter the fourth stage of human development: ")
+stage5 = input("Enter the fifth stage of human development: ")
+stage6 = input("Enter the sixth stage of human development: ")
+
+# "=>"
+print(stage1, stage2, stage3, stage4, stage5, stage6, sep=" => ")
---
+++ учеба питон/PythonLession1.py
@@ -0,0 +1,2 @@
+print("This is a", input("Enter the type of pet: "), "named", input("Enter the pet's name: ") + ".", "Age:", input("Enter the pet's age: "), "years old.")
+
